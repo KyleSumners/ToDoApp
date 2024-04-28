86f8a8a3cb30607cba4ff933afdeb8a0a28e94fa
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-04-28 02:07:22 -0500

    Add ability for user to modify todos

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ todos = []
 
 while True:
     # Get input and strip any whitespace
-    user_action = input("Type add, show, or exit: ")
+    user_action = input("Type add, show, edit, or exit: ")
     user_action = user_action.strip()
     # Perform necessary action on To do list
     match user_action:
@@ -11,7 +11,11 @@ while True:
             todos.append(todo)
         case "show":
             for item in todos:
-                print(item)
+                print(item.capitalize())
+        case "edit":
+            number = int(input("Which todo do you want to edit (number): "))
+            number = number - 1
+            todos[number] = input("Enter the new todo")
         case "exit":
             break
 
