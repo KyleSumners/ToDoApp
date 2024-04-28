23809b54cec6c8bf3cb6307ac74288d1a630fc0f
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-04-28 01:41:35 -0500

    Implement ability to add and show multiple todo list items on command line

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
-print("Enter a todo:")
-input()
+todos = []
+
+while True:
+    # Get input and strip any whitespace
+    user_action = input("Type add, show, or exit: ")
+    user_action = user_action.strip()
+    # Perform necessary action on To do list
+    match user_action:
+        case "add":
+            todo = input("Enter a todo: ")
+            todos.append(todo)
+        case "show":
+            for item in todos:
+                print(item)
+        case "exit":
+            break
+
+print("Done")
