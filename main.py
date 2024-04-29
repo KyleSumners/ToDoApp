525b64663974e2558df0fe89e6588b86f137effe
KyleSumners <114194918+KyleSumners@users.noreply.github.com>
2024-04-28 23:50:00 -0500

    Change program to open files using with context manager

--- main.py
+++ main.py
@@ -2,31 +2,52 @@ while True:
     # Get input and strip any whitespace
     user_action = input("Type add, show, edit, complete, or exit: ")
     user_action = user_action.strip()
+
     # Perform necessary action on To do list
     match user_action:
         case "add":
             todo = input("Enter a todo: ") + "\n"
 
-            file = open("todos.txt", "r")
-            todos = file.readlines()
-            file.close()
+            with open("todos.txt", 'r') as file:
+                todos = file.readlines()
 
             todos.append(todo)
 
-            file = open('todos.txt', "w")
-            file.writelines(todos)
-            file.close()
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+
         case "show":
+            with open("todos.txt", 'r') as file:
+                todos = file.readlines()
+
+
             for index, item in enumerate(todos):
+                item = item.strip('\n')
                 row = f"{index+1}-{item}"
                 print(row)
+
         case "edit":
             number = int(input("Which todo do you want to edit (number): "))
             number = number - 1
-            todos[number] = input("Enter the new todo")
+
+            with open("todos.txt", 'r') as file:
+                todos = file.readlines()
+
+            todos[number] = input("Enter the new todo: ")
+
+            with open("todos.txt", 'w') as file:
+                file.writelines(todos)
         case "complete":
             number = int(input("Number of the todo to complete: "))
+
+            with open("todos.txt", 'r') as file:
+                todos = file.readlines()
+
             todos.pop(number-1)
+
+            with open("todos.txt", 'w') as file:
+                file.writelines(todos)
+            
         case "exit":
             break
 
